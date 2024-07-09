a6abe5f6d384428f742a3b763383ea0ec85440be
Collins <jbcollins254@gmail.com>
2024-07-09 15:05:25 +0300

    fourth commit

--- server/logs.py
+++ server/logs.py
@@ -4,7 +4,8 @@ from flask import Blueprint, request
 from flask_restful import Api, Resource, reqparse
 from config import db, bcrypt, jwt  # Import db from config.py
 from models import User
-from flask_jwt_extended import  create_access_token
+from flask_jwt_extended import  create_access_token ,create_refresh_token
+
 
 # Initialize Blueprint
 logs_bp = Blueprint('logs_bp', __name__, url_prefix='/logs')
@@ -26,7 +27,7 @@ login_args = reqparse.RequestParser()
 login_args.add_argument('email', type=str, help="Email is required", required=True)
 login_args.add_argument('password', type=str, help="Password is required", required=True)
 
-# Define the Signup resource
+
 class Signup(Resource):
     def post(self):
         data = signup_args.parse_args()
@@ -37,11 +38,11 @@ class Signup(Resource):
             db.session.commit()
             return {"msg": "User created successfully"}
         except Exception as e:
-            print(f"Error: {e}")  # Print the exception for debugging
+            print(f"Error: {e}")  
             db.session.rollback()
             return {"msg": "Internal server error"}, 500
 
-# Define the Login resource
+
 class Login(Resource):
     def post(self):
         data = login_args.parse_args()
@@ -50,10 +51,14 @@ class Login(Resource):
             return {"msg": "User does not exist"}, 401
         if not bcrypt.check_password_hash(user.password, data['password']):
             return {"msg": "Wrong password"}, 401
-        token = create_access_token(identity=user.id)
-        return {"username":user.username,
-            "token": token}
+        access_token = create_access_token(identity=user.id)
+        refresh_token =  create_refresh_token(identity = user.id)
 
-# Add the resources to the API
+        return {"username":user.username,
+            "access_token": access_token,
+            "refresh_token": refresh_token
+            }
+
+
 logs_api.add_resource(Signup, '/signup')
 logs_api.add_resource(Login, '/login')
